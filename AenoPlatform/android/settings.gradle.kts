43b4c231c45e53a0b4aa823f256ac5c3aca447c8
BeanFlame (豆焰) <beanflame@qq.com>
2024-05-24 20:07:10 +0800

    Add files via upload

---
+++ AenoPlatform/android/settings.gradle.kts
@@ -0,0 +1,32 @@
+pluginManagement {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        // maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+
+        maven {  url = uri("https://maven.aliyun.com/repository/central") }
+
+
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        // maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+
+        maven {  url = uri("https://maven.aliyun.com/repository/central") }
+
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Game My ApplicationTest"
+include(":app")
